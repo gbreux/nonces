6cc9dd41863bf1b2a012f5ba4e7d34b5e114c27b
g <8329354+gbreux@users.noreply.github.com>
2021-11-29 17:48:09 +0100

    Remove bulkadd

--- models/populate.tsx
+++ models/populate.tsx
@@ -5,42 +5,42 @@ export async function populate() {
 		title: "all",
 	});
 
-	await db.nonce.bulkAdd([
-		{
-			noncesId,
-			uid: "google",
-			title: "Google",
-			meta: {
-				website: { secret: false, value: "https://google.com" },
-				password: {
-					secret: true,
-					value: "azerty",
-				},
-			},
-		},
-		{
-			noncesId,
-			uid: "facebook",
-			title: "Facebook",
-			meta: {
-				website: { secret: false, value: "https://facebook.com" },
-				password: {
-					secret: true,
-					value: "azerty",
-				},
-			},
-		},
-		{
-			noncesId,
-			uid: "myspace",
-			title: "Myspace",
-			meta: {
-				website: { secret: false, value: "https://myspace.com" },
-				password: {
-					secret: true,
-					value: "azerty",
-				},
-			},
-		},
-	]);
+	// await db.nonce.bulkAdd([
+	// 	{
+	// 		noncesId,
+	// 		uid: "google",
+	// 		title: "Google",
+	// 		meta: {
+	// 			website: { secret: false, value: "https://google.com" },
+	// 			password: {
+	// 				secret: true,
+	// 				value: "azerty",
+	// 			},
+	// 		},
+	// 	},
+	// 	{
+	// 		noncesId,
+	// 		uid: "facebook",
+	// 		title: "Facebook",
+	// 		meta: {
+	// 			website: { secret: false, value: "https://facebook.com" },
+	// 			password: {
+	// 				secret: true,
+	// 				value: "azerty",
+	// 			},
+	// 		},
+	// 	},
+	// 	{
+	// 		noncesId,
+	// 		uid: "myspace",
+	// 		title: "Myspace",
+	// 		meta: {
+	// 			website: { secret: false, value: "https://myspace.com" },
+	// 			password: {
+	// 				secret: true,
+	// 				value: "azerty",
+	// 			},
+	// 		},
+	// 	},
+	// ]);
 }
