e873949310dc97fa8b74a3e67975b03f023004ac
rooben-me <rubankumar.nkl@gmail.com>
2021-12-04 03:13:08 +0530

    added truncate to texts

--- components/List.tsx
+++ components/List.tsx
@@ -12,145 +12,145 @@ import initDb from "models/db";
 import GlobalLink from "./GlobalLink";
 
 export default function List({ i18n }) {
-	const router = useRouter();
-	const [search, setsearch] = useState("");
-	const [openModal, setopenModal] = useState(false);
-	const items = useLiveQuery(async () => {
-		const db = await initDb();
-		const result = await db?.nonce?.toArray();
-		if (search) {
-			const searchResult = result.filter(({ title, meta }) => {
-				if (meta) {
-					return (
-						title.toLowerCase().indexOf(search) >= 0 ||
-						Object.keys(meta).filter((key) => {
-							return (
-								!meta[key]?.secret &&
-								meta[key]?.value?.toLowerCase().indexOf(search) >= 0
-							);
-						}).length > 0
-					);
-				} else {
-					return [];
-				}
-			});
-			return [...(searchResult || [])];
-		} else {
-			return [...(result || [])].filter(({ meta }) => !!meta);
-		}
-	}, [search]);
+  const router = useRouter();
+  const [search, setsearch] = useState("");
+  const [openModal, setopenModal] = useState(false);
+  const items = useLiveQuery(async () => {
+    const db = await initDb();
+    const result = await db?.nonce?.toArray();
+    if (search) {
+      const searchResult = result.filter(({ title, meta }) => {
+        if (meta) {
+          return (
+            title.toLowerCase().indexOf(search) >= 0 ||
+            Object.keys(meta).filter((key) => {
+              return (
+                !meta[key]?.secret &&
+                meta[key]?.value?.toLowerCase().indexOf(search) >= 0
+              );
+            }).length > 0
+          );
+        } else {
+          return [];
+        }
+      });
+      return [...(searchResult || [])];
+    } else {
+      return [...(result || [])].filter(({ meta }) => !!meta);
+    }
+  }, [search]);
 
-	useEffect(() => {
-		const $active = document.querySelector(`[href="${router.asPath}"]`);
-		if ($active && !isElementInViewport($active)) {
-			$active?.scrollIntoView({ block: "center" });
-		}
-	}, [router.asPath]);
+  useEffect(() => {
+    const $active = document.querySelector(`[href="${router.asPath}"]`);
+    if ($active && !isElementInViewport($active)) {
+      $active?.scrollIntoView({ block: "center" });
+    }
+  }, [router.asPath]);
 
-	useEffect(() => {
-		if (items && !items?.length) {
-			setopenModal(true);
-		}
-	}, [items]);
+  useEffect(() => {
+    if (items && !items?.length) {
+      setopenModal(true);
+    }
+  }, [items]);
 
-	return (
-		<div className="flex flex-col overflow-auto h-screen md:border-r">
-			<form className="p-4 sticky top-0 bg-gray-100 border-b ">
-				<input
-					className="rounded-lg bg-white border w-full py-2 px-4 focus:outline-none"
-					placeholder="i.e Google"
-					onChange={(e) => {
-						setsearch(e.target.value);
-					}}
-				/>
-			</form>
-			<ul className="p-4 space-y-1">
-				{items?.map(({ title, uid, meta }) => {
-					const path = `/[lang]/nonce/[id]`;
-					const value = Object.keys(meta || {})
-						.filter((key) => !meta?.[key].secret)
-						.map((key) => meta[key])[0]?.value;
-					return (
-						<li
-							className={`relative group p-2 hover:bg-gray-100 rounded-md flex items-center focus-within:bg-gray-100 ${
-								router.query.id == uid ? "bg-gray-100" : ""
-							}`}
-							key={uid}
-						>
-							<svg width="40" viewBox="0 0 40 40" style={{ minWidth: 40 }}>
-								<rect
-									width="40"
-									height="40"
-									rx="10"
-									fill={stringToColour(title)}
-								/>
-								<circle cx="7" cy="7" r="2" fill="white" />
-								<circle cx="33" cy="7" r="2" fill="white" />
-								<circle cx="7" cy="33" r="2" fill="white" />
-								<circle cx="33" cy="33" r="2" fill="white" />
-							</svg>
-							<div className="ml-2">
-								<Link href={path} params={{ id: uid }}>
-									<GlobalLink hideFocus>
-										<Typography variant="h5" className="font-bold">
-											{title}
-										</Typography>
-									</GlobalLink>
-								</Link>
-								<Typography variant="sp" className="text-gray-600">
-									{value}
-								</Typography>
-							</div>
-							<button
-								className={`relative z-10 ml-auto w-10 h-10 rounded-full items-center text-gray-700 justify-center hover:bg-gray-200 ${
-									router.query.id == uid ? "flex" : "hidden group-hover:flex"
-								}`}
-								onClick={() => {
-									initDb().then((db) => {
-										if (router.query.id === uid) {
-											goToRoute("/[lang]/nonce", router.query.lang);
-										}
-										db.nonce.where("uid").equals(uid).delete();
-									});
-								}}
-							>
-								<Trash className="w-5 h-5" />
-							</button>
-						</li>
-					);
-				})}
-			</ul>
-			<footer className="bg-white sticky bottom-0 p-4 mt-auto">
-				<Button className="w-full" onClick={() => setopenModal(true)}>
-					{i18n.cta}
-				</Button>
-				<NonceDialog
-					i18n={i18n.Components.NonceDialog}
-					isOpen={openModal}
-					close={() => setopenModal(false)}
-					onSubmit={(item) => {
-						initDb().then((db) => db.nonce.add({ ...item }));
-						goToRoute("/[lang]/nonce/[id]", router.query.lang, {
-							id: item.uid,
-						});
-					}}
-				/>
-			</footer>
-		</div>
-	);
+  return (
+    <div className="flex flex-col overflow-auto h-screen md:border-r">
+      <form className="p-4 sticky top-0 bg-gray-100 border-b ">
+        <input
+          className="rounded-lg bg-white border w-full py-2 px-4 focus:outline-none"
+          placeholder="i.e Google"
+          onChange={(e) => {
+            setsearch(e.target.value);
+          }}
+        />
+      </form>
+      <ul className="p-4 space-y-1">
+        {items?.map(({ title, uid, meta }) => {
+          const path = `/[lang]/nonce/[id]`;
+          const value = Object.keys(meta || {})
+            .filter((key) => !meta?.[key].secret)
+            .map((key) => meta[key])[0]?.value;
+          return (
+            <li
+              className={`relative group p-2 hover:bg-gray-100 rounded-md flex items-center focus-within:bg-gray-100 ${
+                router.query.id == uid ? "bg-gray-100" : ""
+              }`}
+              key={uid}
+            >
+              <svg width="40" viewBox="0 0 40 40" style={{ minWidth: 40 }}>
+                <rect
+                  width="40"
+                  height="40"
+                  rx="10"
+                  fill={stringToColour(title)}
+                />
+                <circle cx="7" cy="7" r="2" fill="white" />
+                <circle cx="33" cy="7" r="2" fill="white" />
+                <circle cx="7" cy="33" r="2" fill="white" />
+                <circle cx="33" cy="33" r="2" fill="white" />
+              </svg>
+              <div className="ml-2 w-3/5">
+                <Link href={path} params={{ id: uid }}>
+                  <GlobalLink hideFocus>
+                    <Typography variant="h5" className="font-bold truncate">
+                      {title}
+                    </Typography>
+                  </GlobalLink>
+                </Link>
+                <Typography variant="sp" className="text-gray-600 truncate">
+                  {value}
+                </Typography>
+              </div>
+              <button
+                className={`relative z-10 ml-auto w-10 h-10 rounded-full items-center text-gray-700 justify-center hover:bg-gray-200 ${
+                  router.query.id == uid ? "flex" : "hidden group-hover:flex"
+                }`}
+                onClick={() => {
+                  initDb().then((db) => {
+                    if (router.query.id === uid) {
+                      goToRoute("/[lang]/nonce", router.query.lang);
+                    }
+                    db.nonce.where("uid").equals(uid).delete();
+                  });
+                }}
+              >
+                <Trash className="w-5 h-5" />
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      <footer className="bg-white sticky bottom-0 p-4 mt-auto">
+        <Button className="w-full" onClick={() => setopenModal(true)}>
+          {i18n.cta}
+        </Button>
+        <NonceDialog
+          i18n={i18n.Components.NonceDialog}
+          isOpen={openModal}
+          close={() => setopenModal(false)}
+          onSubmit={(item) => {
+            initDb().then((db) => db.nonce.add({ ...item }));
+            goToRoute("/[lang]/nonce/[id]", router.query.lang, {
+              id: item.uid,
+            });
+          }}
+        />
+      </footer>
+    </div>
+  );
 }
 
 function isElementInViewport(el) {
-	var rect = el.getBoundingClientRect();
+  var rect = el.getBoundingClientRect();
 
-	return (
-		rect.bottom > 0 &&
-		rect.right > 0 &&
-		rect.left <
-			(window.innerWidth ||
-				document.documentElement.clientWidth) /* or $(window).width() */ &&
-		rect.top <
-			(window.innerHeight ||
-				document.documentElement.clientHeight) /* or $(window).height() */
-	);
+  return (
+    rect.bottom > 0 &&
+    rect.right > 0 &&
+    rect.left <
+      (window.innerWidth ||
+        document.documentElement.clientWidth) /* or $(window).width() */ &&
+    rect.top <
+      (window.innerHeight ||
+        document.documentElement.clientHeight) /* or $(window).height() */
+  );
 }
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -12,132 +12,132 @@ import { stringToColour } from "lib/utils";
 import initDb from "models/db";
 
 export default function ListItem({ i18n }) {
-	const [openNonceDialog, setopenNonceDialog] = useState(false);
-	const [copied, setcopied] = useState("");
-	const router = useRouter();
-	// const item = [];
-	const item = useLiveQuery(async () => {
-		const db = await initDb();
-		return await db
-			?.table("nonce")
-			?.filter(({ uid }) => {
-				return uid === router?.query?.id;
-			})
-			.toArray();
-	}, [router.query.id])?.[0];
+  const [openNonceDialog, setopenNonceDialog] = useState(false);
+  const [copied, setcopied] = useState("");
+  const router = useRouter();
+  // const item = [];
+  const item = useLiveQuery(async () => {
+    const db = await initDb();
+    return await db
+      ?.table("nonce")
+      ?.filter(({ uid }) => {
+        return uid === router?.query?.id;
+      })
+      .toArray();
+  }, [router.query.id])?.[0];
 
-	return (
-		<div className="flex flex-col overflow-auto h-screen">
-			<header className="p-4 flex items-center">
-				<svg width="40" viewBox="0 0 40 40">
-					<rect
-						width="40"
-						height="40"
-						rx="10"
-						fill={stringToColour(item?.title || "")}
-					/>
-					<circle cx="7" cy="7" r="2" fill="white" />
-					<circle cx="33" cy="7" r="2" fill="white" />
-					<circle cx="7" cy="33" r="2" fill="white" />
-					<circle cx="33" cy="33" r="2" fill="white" />
-				</svg>
-				<div className="ml-2">
-					<Typography variant="h5" className="font-bold">
-						{item?.title}
-					</Typography>
-					{Object.keys(item?.meta || {})
-						.filter(
-							(key) => (item?.meta[key]?.value || "")?.indexOf("http") >= 0
-						)
-						.map((key) => {
-							return (
-								<Typography key={key} variant="sp" className="text-gray-600">
-									{item?.meta[key]?.value}
-								</Typography>
-							);
-						})}
-				</div>
-				<Button
-					className="ml-auto flex items-center text-sm"
-					rounded
-					small
-					onClick={() => setopenNonceDialog(true)}
-				>
-					<Edit className="w-4 h-4 mr-2" />
-					Edit
-				</Button>
-			</header>
-			<section className="px-4">
-				<ul className="rounded-lg border overflow-hidden">
-					{Object.keys(item?.meta || {}).map((key, index) => {
-						const { value = "", secret = false } = item?.meta[key] || {};
-						return (
-							<li
-								className="relative p-2 border-b last:border-none cursor-pointer hover:bg-gray-100"
-								key={key}
-								onClick={() => {
-									navigator.clipboard.writeText(value);
-									setcopied(key);
-									setTimeout(() => setcopied(""), 3000);
-								}}
-							>
-								<Typography variant="h5" className="font-bold">
-									{key}
-								</Typography>
-								{/^http/.test(value) && !secret ? (
-									<Typography
-										component="a"
-										variant="sp"
-										className="inline-flex items-center underline text-gray-600 hover:text-blue-600"
-										href={value}
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										{value}
-										<Link className="w-4 h-4 ml-1" />
-									</Typography>
-								) : (
-									<Typography variant="sp" className="text-gray-600">
-										{secret ? "••••••••" : value}
-									</Typography>
-								)}
-								<aside className="absolute right-2 top-1/2 transform -translate-y-1/2">
-									{copied === key ? (
-										<Typography
-											variant="sp"
-											className=" text-green-500 font-bold"
-										>
-											{i18n.copied}
-										</Typography>
-									) : (
-										<button className="w-10 h-10 flex items-center justify-center rounded-full bg-gray-600 bg-opacity-0 hover:bg-opacity-10 focus:bg-opacity-5">
-											<Copy
-												className="w-5 h-5 text-gray-700"
-												titleId={`Copy${index}`}
-												title={`Copy ${key}`}
-											/>
-										</button>
-									)}
-								</aside>
-							</li>
-						);
-					})}
-				</ul>
-			</section>
-			<NonceDialog
-				i18n={i18n.Components.NonceDialog}
-				isOpen={openNonceDialog}
-				close={() => setopenNonceDialog(false)}
-				defaultValues={item}
-				onSubmit={async (item) => {
-					await initDb().then((db) =>
-						db.nonce
-							.where("uid")
-							.equals(router.query.id || "")
-							.modify({ ...item })
-					);
-				}}
-			/>
-		</div>
-	);
+  return (
+    <div className="flex flex-col overflow-auto h-screen">
+      <header className="p-4 flex items-center">
+        <svg width="40" viewBox="0 0 40 40">
+          <rect
+            width="40"
+            height="40"
+            rx="10"
+            fill={stringToColour(item?.title || "")}
+          />
+          <circle cx="7" cy="7" r="2" fill="white" />
+          <circle cx="33" cy="7" r="2" fill="white" />
+          <circle cx="7" cy="33" r="2" fill="white" />
+          <circle cx="33" cy="33" r="2" fill="white" />
+        </svg>
+        <div className="ml-2 w-3/5">
+          <Typography variant="h5" className="font-bold truncate">
+            {item?.title}
+          </Typography>
+          {Object.keys(item?.meta || {})
+            .filter(
+              (key) => (item?.meta[key]?.value || "")?.indexOf("http") >= 0
+            )
+            .map((key) => {
+              return (
+                <Typography key={key} variant="sp" className="text-gray-600">
+                  {item?.meta[key]?.value}
+                </Typography>
+              );
+            })}
+        </div>
+        <Button
+          className="ml-auto flex items-center text-sm"
+          rounded
+          small
+          onClick={() => setopenNonceDialog(true)}
+        >
+          <Edit className="w-4 h-4 mr-2" />
+          Edit
+        </Button>
+      </header>
+      <section className="px-4">
+        <ul className="rounded-lg border overflow-hidden">
+          {Object.keys(item?.meta || {}).map((key, index) => {
+            const { value = "", secret = false } = item?.meta[key] || {};
+            return (
+              <li
+                className="relative p-2 border-b last:border-none cursor-pointer hover:bg-gray-100"
+                key={key}
+                onClick={() => {
+                  navigator.clipboard.writeText(value);
+                  setcopied(key);
+                  setTimeout(() => setcopied(""), 3000);
+                }}
+              >
+                <Typography variant="h5" className="font-bold">
+                  {key}
+                </Typography>
+                {/^http/.test(value) && !secret ? (
+                  <Typography
+                    component="a"
+                    variant="sp"
+                    className="inline-flex items-center underline text-gray-600 hover:text-blue-600"
+                    href={value}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {value}
+                    <Link className="w-4 h-4 ml-1" />
+                  </Typography>
+                ) : (
+                  <Typography variant="sp" className="text-gray-600">
+                    {secret ? "••••••••" : value}
+                  </Typography>
+                )}
+                <aside className="absolute right-2 top-1/2 transform -translate-y-1/2">
+                  {copied === key ? (
+                    <Typography
+                      variant="sp"
+                      className=" text-green-500 font-bold"
+                    >
+                      {i18n.copied}
+                    </Typography>
+                  ) : (
+                    <button className="w-10 h-10 flex items-center justify-center rounded-full bg-gray-600 bg-opacity-0 hover:bg-opacity-10 focus:bg-opacity-5">
+                      <Copy
+                        className="w-5 h-5 text-gray-700"
+                        titleId={`Copy${index}`}
+                        title={`Copy ${key}`}
+                      />
+                    </button>
+                  )}
+                </aside>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+      <NonceDialog
+        i18n={i18n.Components.NonceDialog}
+        isOpen={openNonceDialog}
+        close={() => setopenNonceDialog(false)}
+        defaultValues={item}
+        onSubmit={async (item) => {
+          await initDb().then((db) =>
+            db.nonce
+              .where("uid")
+              .equals(router.query.id || "")
+              .modify({ ...item })
+          );
+        }}
+      />
+    </div>
+  );
 }
