3e327123b7efe48dc076ea8df315e37dcf15d0be
rooben-me <rubankumar.nkl@gmail.com>
2021-12-04 03:57:58 +0530

    added toggle visible in main bar

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -10,10 +10,13 @@ import Button from "components/Button";
 import Typography from "components/Typography";
 import { stringToColour } from "lib/utils";
 import initDb from "models/db";
+import EyeOff from "./Icons/EyeOff";
+import EyeOn from "./Icons/EyeOn";
 
 export default function ListItem({ i18n }) {
   const [openNonceDialog, setopenNonceDialog] = useState(false);
   const [copied, setcopied] = useState("");
+  const [showSecret, setshowSecret] = useState(false);
   const router = useRouter();
   // const item = [];
   const item = useLiveQuery(async () => {
@@ -41,7 +44,7 @@ export default function ListItem({ i18n }) {
           <circle cx="7" cy="33" r="2" fill="white" />
           <circle cx="33" cy="33" r="2" fill="white" />
         </svg>
-        <div className="ml-2 w-3/5">
+        <div className="ml-2 w-3/4">
           <Typography variant="h5" className="font-bold truncate">
             {item?.title}
           </Typography>
@@ -71,37 +74,51 @@ export default function ListItem({ i18n }) {
         <ul className="rounded-lg border overflow-hidden">
           {Object.keys(item?.meta || {}).map((key, index) => {
             const { value = "", secret = false } = item?.meta[key] || {};
+
+            console.log(value);
             return (
               <li
-                className="relative p-2 border-b last:border-none cursor-pointer hover:bg-gray-100"
+                className="relative p-2 border-b last:border-none cursor-pointer hover:bg-gray-100 flex justify-between items-center"
                 key={key}
-                onClick={() => {
-                  navigator.clipboard.writeText(value);
-                  setcopied(key);
-                  setTimeout(() => setcopied(""), 3000);
-                }}
               >
-                <Typography variant="h5" className="font-bold">
-                  {key}
-                </Typography>
-                {/^http/.test(value) && !secret ? (
-                  <Typography
-                    component="a"
-                    variant="sp"
-                    className="inline-flex items-center underline text-gray-600 hover:text-blue-600"
-                    href={value}
-                    target="_blank"
-                    rel="noopener noreferrer"
+                <div className="flex flex-col items-start w-4/5">
+                  <Typography variant="h5" className="font-bold">
+                    {key}
+                  </Typography>
+                  {/^http/.test(value) && !secret ? (
+                    <Typography
+                      component="a"
+                      variant="sp"
+                      className="inline-flex items-center underline text-gray-600 hover:text-blue-"
+                      href={value}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {value}
+                      <Link className="w-4 h-4 ml-1" />
+                    </Typography>
+                  ) : (
+                    <Typography
+                      variant="sp"
+                      className="text-gray-600 w-full break-words"
+                    >
+                      {!showSecret ? "••••••••" : value}
+                    </Typography>
+                  )}
+                </div>
+                <aside className="flex gap-x-2 items-center">
+                  <button
+                    onClick={() => {
+                      setshowSecret(!showSecret);
+                    }}
+                    className="w-10 h-10 flex items-center justify-center rounded-full bg-gray-600 bg-opacity-0 hover:bg-opacity-10 focus:bg-opacity-5"
                   >
-                    {value}
-                    <Link className="w-4 h-4 ml-1" />
-                  </Typography>
-                ) : (
-                  <Typography variant="sp" className="text-gray-600">
-                    {secret ? "••••••••" : value}
-                  </Typography>
-                )}
-                <aside className="absolute right-2 top-1/2 transform -translate-y-1/2">
+                    {!showSecret ? (
+                      <EyeOff className="w-5 h-5" />
+                    ) : (
+                      <EyeOn className="w-5 h-5" />
+                    )}
+                  </button>
                   {copied === key ? (
                     <Typography
                       variant="sp"
@@ -110,7 +127,14 @@ export default function ListItem({ i18n }) {
                       {i18n.copied}
                     </Typography>
                   ) : (
-                    <button className="w-10 h-10 flex items-center justify-center rounded-full bg-gray-600 bg-opacity-0 hover:bg-opacity-10 focus:bg-opacity-5">
+                    <button
+                      onClick={() => {
+                        navigator.clipboard.writeText(value);
+                        setcopied(key);
+                        setTimeout(() => setcopied(""), 3000);
+                      }}
+                      className="w-10 h-10 flex items-center justify-center rounded-full bg-gray-600 bg-opacity-0 hover:bg-opacity-10 focus:bg-opacity-5"
+                    >
                       <Copy
                         className="w-5 h-5 text-gray-700"
                         titleId={`Copy${index}`}
