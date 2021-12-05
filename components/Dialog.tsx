eacdb724c3ad43edf0e90cda449657e93d1cd8ad
ruban-me <rubankumar.nkl@gmail.com>
2021-12-05 21:09:43 +0530

    added confirmation for delete

--- components/Dialog.tsx
+++ components/Dialog.tsx
@@ -4,35 +4,38 @@ import SvgClose from "components/Icons/Close";
 import useNoScroll from "hooks/useNoScroll.hook";
 
 export default function Modal({
-	isOpen,
-	close,
-	children,
-	width = 750,
-	hideCloseButton = false,
-	...props
+    isOpen,
+    close,
+    children,
+    width = 750,
+    hideCloseButton = false,
+    ...props
 }) {
-	useNoScroll(isOpen);
+    useNoScroll(isOpen);
 
-	return (
-		<ReactModal
-			isOpen={isOpen}
-			onRequestClose={close}
-			width={width}
-			className="cursor-auto bg-white max-w-3xl h-full md:h-auto md:my-auto w-full md:rounded-2xl md:shadow-2xl relative focus:outline-none"
-			overlayClassName="fixed inset-0 items-start md:p-4 justify-center flex h-full overflow-auto z-50 cursor-pointer bg-gray-900 bg-opacity-90"
-			{...props}
-		>
-			<div style={{ borderRadius: "inherit" }} className="p-4 md:p-7">
-				{children}
-				{close && !hideCloseButton ? (
-					<button
-						className="absolute focus:outline-black rounded-full p-2 right-2 top-2 flex hover:text-gray-500 text-gray-300 md:right-4 md:top-4 md:text-2xl"
-						onClick={close}
-					>
-						<SvgClose className="w-6 h-6" />
-					</button>
-				) : null}
-			</div>
-		</ReactModal>
-	);
+    return (
+        <ReactModal
+            isOpen={isOpen}
+            onRequestClose={close}
+            width={width}
+            className="cursor-auto bg-white max-w-3xl h-full md:h-auto md:my-auto w-full md:rounded-2xl md:shadow-2xl relative focus:outline-none"
+            overlayClassName="fixed inset-0 items-start md:p-4 justify-center flex h-full overflow-auto z-50 cursor-pointer bg-gray-900 bg-opacity-90"
+            {...props}
+        >
+            <div
+                style={{ borderRadius: "inherit" }}
+                className="p-4 md:p-7 h-full"
+            >
+                {children}
+                {close && !hideCloseButton ? (
+                    <button
+                        className="absolute focus:outline-black rounded-full p-2 right-2 top-2 flex hover:text-gray-500 text-gray-300 md:right-4 md:top-4 md:text-2xl"
+                        onClick={close}
+                    >
+                        <SvgClose className="w-6 h-6" />
+                    </button>
+                ) : null}
+            </div>
+        </ReactModal>
+    );
 }
--- components/List.tsx
+++ components/List.tsx
@@ -9,142 +9,173 @@ import Trash from "components/Icons/Trash";
 import NonceDialog from "components/Dialog/NonceDialog";
 import { goToRoute, stringToColour } from "lib/utils";
 import GlobalLink from "./GlobalLink";
+import ConfirmationDialog from "./Dialog/ConfirmationDialog";
 
 export default function List({ i18n, db }) {
-	const router = useRouter();
-	const [search, setsearch] = useState("");
-	const [openModal, setopenModal] = useState(false);
-	const items = useLiveQuery(async () => {
-		const result = await db?.nonce?.toArray();
-		if (search) {
-			const searchResult = result.filter(({ title, meta }) => {
-				if (meta) {
-					return (
-						title.toLowerCase().indexOf(search) >= 0 ||
-						Object.keys(meta).filter((key) => {
-							return (
-								!meta[key]?.secret &&
-								meta[key]?.value?.toLowerCase().indexOf(search) >= 0
-							);
-						}).length > 0
-					);
-				} else {
-					return [];
-				}
-			});
-			return [...(searchResult || [])];
-		} else {
-			return [...(result || [])].filter(({ meta }) => !!meta);
-		}
-	}, [search]);
+    const router = useRouter();
+    const [search, setsearch] = useState("");
+    const [openModal, setopenModal] = useState(false);
+    const [openConfirmation, setopenConfirmation] = useState(false);
+    const items = useLiveQuery(async () => {
+        const result = await db?.nonce?.toArray();
+        if (search) {
+            const searchResult = result.filter(({ title, meta }) => {
+                if (meta) {
+                    return (
+                        title.toLowerCase().indexOf(search) >= 0 ||
+                        Object.keys(meta).filter((key) => {
+                            return (
+                                !meta[key]?.secret &&
+                                meta[key]?.value
+                                    ?.toLowerCase()
+                                    .indexOf(search) >= 0
+                            );
+                        }).length > 0
+                    );
+                } else {
+                    return [];
+                }
+            });
+            return [...(searchResult || [])];
+        } else {
+            return [...(result || [])].filter(({ meta }) => !!meta);
+        }
+    }, [search]);
 
-	useEffect(() => {
-		const $active = document.querySelector(`[href="${router.asPath}"]`);
-		if ($active && !isElementInViewport($active)) {
-			$active?.scrollIntoView({ block: "center" });
-		}
-	}, [router.asPath]);
+    useEffect(() => {
+        const $active = document.querySelector(`[href="${router.asPath}"]`);
+        if ($active && !isElementInViewport($active)) {
+            $active?.scrollIntoView({ block: "center" });
+        }
+    }, [router.asPath]);
 
-	useEffect(() => {
-		if (items && !items?.length) {
-			setopenModal(true);
-		}
-	}, [items]);
+    useEffect(() => {
+        if (items && !items?.length) {
+            setopenModal(true);
+        }
+    }, [items]);
 
-	return (
-		<div className="flex flex-col overflow-auto h-screen md:border-r">
-			<form className="p-4 sticky top-0 bg-gray-100 border-b ">
-				<input
-					className="rounded-lg bg-white border w-full py-2 px-4 focus:outline-none"
-					placeholder="i.e Google"
-					onChange={(e) => {
-						setsearch(e.target.value);
-					}}
-				/>
-			</form>
-			<ul className="p-4 space-y-1">
-				{items?.map(({ title, uid, meta }) => {
-					const path = `/[lang]/nonce?id=${uid}`;
-					const value = Object.keys(meta || {})
-						.filter((key) => !meta?.[key].secret)
-						.map((key) => meta[key])[0]?.value;
-					return (
-						<li
-							className={`relative group p-2 hover:bg-gray-100 rounded-md flex items-center focus-within:bg-gray-100 ${
-								router.query.id == uid ? "bg-gray-100" : ""
-							}`}
-							key={uid}
-						>
-							<svg width="40" viewBox="0 0 40 40" style={{ minWidth: 40 }}>
-								<rect
-									width="40"
-									height="40"
-									rx="10"
-									fill={stringToColour(title)}
-								/>
-								<circle cx="7" cy="7" r="2" fill="white" />
-								<circle cx="33" cy="7" r="2" fill="white" />
-								<circle cx="7" cy="33" r="2" fill="white" />
-								<circle cx="33" cy="33" r="2" fill="white" />
-							</svg>
-							<div className="ml-2">
-								<Link href={path}>
-									<GlobalLink hideFocus>
-										<Typography variant="h5" className="font-bold">
-											{title}
-										</Typography>
-									</GlobalLink>
-								</Link>
-								<Typography variant="sp" className="text-gray-600">
-									{value}
-								</Typography>
-							</div>
-							<button
-								className={`relative z-10 ml-auto w-10 h-10 rounded-full items-center text-gray-700 justify-center hover:bg-gray-200 ${
-									router.query.id == uid ? "flex" : "hidden group-hover:flex"
-								}`}
-								onClick={() => {
-									if (router.query.id === uid) {
-										goToRoute("/[lang]/nonce", router.query.lang);
-									}
-									db.nonce.where("uid").equals(uid).delete();
-								}}
-							>
-								<Trash className="w-5 h-5" />
-							</button>
-						</li>
-					);
-				})}
-			</ul>
-			<footer className="bg-white sticky bottom-0 p-4 mt-auto">
-				<Button className="w-full" onClick={() => setopenModal(true)}>
-					{i18n.cta}
-				</Button>
-				<NonceDialog
-					i18n={i18n.Components.NonceDialog}
-					isOpen={openModal}
-					close={() => setopenModal(false)}
-					onSubmit={(item) => {
-						db.nonce.add({ ...item });
-						goToRoute(`/[lang]/nonce?id=${item.uid}`, router.query.lang);
-					}}
-				/>
-			</footer>
-		</div>
-	);
+    return (
+        <div className="flex flex-col overflow-auto h-screen md:border-r">
+            <form className="p-4 sticky top-0 bg-gray-100 border-b z-10">
+                <input
+                    className="rounded-lg bg-white border w-full py-2 px-4 focus:outline-none"
+                    placeholder="i.e Google"
+                    onChange={(e) => {
+                        setsearch(e.target.value);
+                    }}
+                />
+            </form>
+            <ul className="p-4 space-y-1">
+                {items?.map(({ title, uid, meta }) => {
+                    const path = `/[lang]/nonce?id=${uid}`;
+                    const value = Object.keys(meta || {})
+                        .filter((key) => !meta?.[key].secret)
+                        .map((key) => meta[key])[0]?.value;
+                    return (
+                        <li
+                            className={`relative group p-2 hover:bg-gray-100 rounded-md flex items-center focus-within:bg-gray-100 ${
+                                router.query.id == uid ? "bg-gray-100" : ""
+                            }`}
+                            key={uid}
+                        >
+                            <svg
+                                width="40"
+                                viewBox="0 0 40 40"
+                                style={{ minWidth: 40 }}
+                            >
+                                <rect
+                                    width="40"
+                                    height="40"
+                                    rx="10"
+                                    fill={stringToColour(title)}
+                                />
+                                <circle cx="7" cy="7" r="2" fill="white" />
+                                <circle cx="33" cy="7" r="2" fill="white" />
+                                <circle cx="7" cy="33" r="2" fill="white" />
+                                <circle cx="33" cy="33" r="2" fill="white" />
+                            </svg>
+                            <div className="ml-2">
+                                <Link href={path}>
+                                    <GlobalLink hideFocus>
+                                        <Typography
+                                            variant="h5"
+                                            className="font-bold"
+                                        >
+                                            {title}
+                                        </Typography>
+                                    </GlobalLink>
+                                </Link>
+                                <Typography
+                                    variant="sp"
+                                    className="text-gray-600"
+                                >
+                                    {value}
+                                </Typography>
+                            </div>
+                            <button
+                                className={`relative z-10 ml-auto w-10 h-10 rounded-full items-center text-gray-700 justify-center hover:bg-gray-200 hover:text-red-500 ${
+                                    router.query.id == uid
+                                        ? "flex"
+                                        : "hidden group-hover:flex"
+                                }`}
+                                onClick={() => setopenConfirmation(true)}
+                            >
+                                <Trash className="w-5 h-5" />
+                            </button>
+                            <ConfirmationDialog
+                                isOpen={openConfirmation}
+                                close={() => setopenConfirmation(false)}
+                                onSubmit={() => {
+                                    if (router.query.id === uid) {
+                                        goToRoute(
+                                            "/[lang]/nonce",
+                                            router.query.lang
+                                        );
+                                    }
+                                    db.nonce.where("uid").equals(uid).delete();
+                                    setopenConfirmation(false);
+                                }}
+                                i18n={i18n.Components.ConfirmationDialog}
+                            />
+                        </li>
+                    );
+                })}
+            </ul>
+            <footer className="bg-white sticky bottom-0 p-4 mt-auto z-10">
+                <Button className="w-full" onClick={() => setopenModal(true)}>
+                    {i18n.cta}
+                </Button>
+                <NonceDialog
+                    i18n={i18n.Components.NonceDialog}
+                    isOpen={openModal}
+                    close={() => setopenModal(false)}
+                    onSubmit={(item) => {
+                        db.nonce.add({ ...item });
+                        goToRoute(
+                            `/[lang]/nonce?id=${item.uid}`,
+                            router.query.lang
+                        );
+                    }}
+                />
+            </footer>
+        </div>
+    );
 }
 
 function isElementInViewport(el) {
-	var rect = el.getBoundingClientRect();
+    var rect = el.getBoundingClientRect();
 
-	return (
-		rect.bottom > 0 &&
-		rect.right > 0 &&
-		rect.left <
-			(window.innerWidth ||
-				document.documentElement.clientWidth) /* or $(window).width() */ &&
-		rect.top <
-			(window.innerHeight ||
-				document.documentElement.clientHeight) /* or $(window).height() */
-	);
+    return (
+        rect.bottom > 0 &&
+        rect.right > 0 &&
+        rect.left <
+            (window.innerWidth ||
+                document.documentElement
+                    .clientWidth) /* or $(window).width() */ &&
+        rect.top <
+            (window.innerHeight ||
+                document.documentElement
+                    .clientHeight) /* or $(window).height() */
+    );
 }
